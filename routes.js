baeea2de875ae092d426a8a68e5abc68a679bd69
MatthewKosloski <matthewkosloski@me.com>
2016-08-14 21:56:14 -0500

    remove temp directory

--- routes.js
+++ routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
 
 const router = express.Router();
 
 const upload = multer({ 
-	dest: 'temp/' ,
+	dest: 'temp',
 	fileFilter: (req, file, cb) => {
 		if(/image\/(jpeg|jpg|png)/.test(file.mimetype)) {
 			cb(null, true);
@@ -16,6 +18,13 @@ const upload = multer({
 	}
 });
 
+router.get('/', (req, res, next) => {
+	res.sendFile(__dirname + '/index.html');
+	mkdirp('temp', (err) => {
+    	if (err) console.error(err)
+	});
+});
+
 router.post('/', upload.single('photo'), (req, res, next) => {
 	const uploadedPhoto = req.file;
 	if(uploadedPhoto) {
@@ -23,17 +32,20 @@ router.post('/', upload.single('photo'), (req, res, next) => {
 			if(err) return next(err);
 			const {filename, mimetype} = uploadedPhoto;
 			const ext = `.${mimetype.substring(mimetype.search(/(?!image\/)(jpeg|jpg|png)/))}`;
-			var filenameWithExt = `${uploadedPhoto.filename}${ext}`;
+			const filenameWithExt = `${uploadedPhoto.filename}${ext}`;
 			fs.writeFile(filenameWithExt, data, {encoding: 'base64'}, (err) => {
 				if(err) return next(err);
 				fs.rename(filenameWithExt, path.join(__dirname, `public/images/${filenameWithExt}`), (err) => {
 					if(err) return next(err);
+					rimraf('temp', (err) => {
+						if (err) console.log(err);
+					});
 					res.redirect('/images/' + filenameWithExt);
 				});
 			});
 		});
 	} else {
-		var err = new Error('Only .jpeg, .jpg, or .png files are supported.');
+		let err = new Error('Only .jpeg, .jpg, or .png files are supported.');
 		err.status = 401;
 		return next(err);
 	}
